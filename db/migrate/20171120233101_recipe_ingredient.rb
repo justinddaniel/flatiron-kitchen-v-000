0004a697d220fe31070f67b41fef902ca9ac49e3
Justin Daniel <justinddaniel@gmail.com>
2017-11-20 23:33:08 +0000

    automatically backed up by learn

--- db/migrate/20171120233101_recipe_ingredient.rb
+++ db/migrate/20171120233101_recipe_ingredient.rb
@@ -1,6 +1,6 @@
 class RecipeIngredient < ActiveRecord::Migration
   def change
-    add_table :recipe_ingredients do |r|
+    create_table :recipe_ingredients do |r|
       r.integer :recipe_id
       r.integer :ingredient_id
   end
